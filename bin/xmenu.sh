b33fb68dc1a1bbb5f7f6af22062da69ed264e01c
philor1 <scheibe.philipp@gmail.com>
2020-09-20 17:18:18 +0200

    update

--- bin/xmenu.sh
+++ bin/xmenu.sh
@@ -2,7 +2,7 @@
 
 cat <<EOF | xmenu | sh &
 Applications
-	IMG:/home/philipp/.xfiles/icons/web-browser-symbolic.png		Browser			qutebrowser
+	IMG:/home/philipp/.xfiles/icons/web-browser-symbolic.png		Browser			firefox
 	IMG:/home/philipp/.xfiles/icons/sidebar-places-symbolic.png		Filemanager		pcmanfm
 
 Terminal (st)		st
